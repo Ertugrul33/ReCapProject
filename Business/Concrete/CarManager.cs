ffb7f5031650bf7c5d507c2ea7d6afa86a1700a7
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-02-05 17:55:08 +0300

    8. Gün Ödev 1

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -15,11 +15,36 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Brand brand, Car car)
+        {
+            if (brand.BrandName.Length > 2 && car.DailyPrice > 0)
+            {
+                Console.WriteLine("Marka başarıyla eklendi: " + brand.BrandName);
+                Console.WriteLine("Günlük Fiyatı: " + car.DailyPrice);
+
+            }
+            else
+            {
+                Console.WriteLine("Araba ismi minimum 2 karakter ve günlük fiyatı 0'dan büyük olmalıdır.");
+            }
+        }
+
         public List<Car> GetAll()
         {
             //İş Kodları
             //...
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -15,11 +16,11 @@ namespace DataAccess.Concrete.InMemory
             _cars = new List<Car>
             {
                 new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=250000,
-                 ModelYear=2019, Description="TOGG: Yeni Lige Yolculuk"},
+                 ModelYear="2019", Descriptions="TOGG: Yeni Lige Yolculuk"},
                 new Car{Id=2, BrandId=2, ColorId=2, DailyPrice=70000,
-                 ModelYear=2015, Description="BMW: Sheer Driving Pleasure"},
+                 ModelYear="2015", Descriptions="BMW: Sheer Driving Pleasure"},
                 new Car{Id=3, BrandId=1, ColorId=1, DailyPrice=70000,
-                 ModelYear=2015, Description="Lamborghini: For Love of the Car."}
+                 ModelYear="2015", Descriptions="Lamborghini: For Love of the Car."}
             };
         }
 
@@ -34,9 +35,19 @@ namespace DataAccess.Concrete.InMemory
             _cars.Remove(carToDelete);
         }
 
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetAll()
         {
             return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetById(int id)
@@ -51,7 +62,7 @@ namespace DataAccess.Concrete.InMemory
             carToUpdate.ColorId = car.ColorId;
             carToUpdate.ModelYear = car.ModelYear;
             carToUpdate.DailyPrice = car.DailyPrice;
-            carToUpdate.Description = car.Description;
+            carToUpdate.Descriptions = car.Descriptions;
         }
     }
 }
