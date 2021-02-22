0ec3d7f69d9a56732944b1c7945bda2f2b521881
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-02-22 12:42:40 +0300

    FluentValidation desteği eklendi.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(ca => ca.CarName).NotEmpty();
+            RuleFor(ca => ca.CarName).MinimumLength(2);
+            RuleFor(ca => ca.DailyPrice).NotEmpty();
+            RuleFor(ca => ca.DailyPrice).GreaterThan(0);
+            RuleFor(ca => ca.DailyPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(ca => ca.CarName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalıdır.");
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+    }
+}
