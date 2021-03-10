f871333c451b35d2e7a58c8eab625a3debc42d25
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-03-10 15:00:53 +0300

    10 Mart 2021 Güncellendi.

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -22,6 +22,12 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ColorAdded);
         }
 
+        public IResult Update(Color color)
+        {
+            _colorDal.Update(color);
+            return new SuccessResult(Messages.ColorUpdated);
+        }
+
         public IResult Delete(Color color)
         {
             _colorDal.Delete(color);
@@ -41,12 +47,6 @@ namespace Business.Concrete
         public IDataResult<Color> GetById(int colorId)
         {
             return new SuccessDataResult<Color>(_colorDal.Get(co => co.ColorId == colorId));
-        }
-
-        public IResult Update(Color color)
-        {
-            _colorDal.Update(color);
-            return new SuccessResult(Messages.ColorUpdated);
         }
     }
 }
